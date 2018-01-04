10f34fc0b7e99ae9350789798092f90f20ede484
Shaowei Zhang <zsw22@vip.qq.com>
2018-01-04 23:56:12 +1100

    import all the component and test bootstrap css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import classes from './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	state = {
+		people: [
+			{name: 'Shaowei', age: 25},
+			{name: 'Kira', age: 22},
+			{name: 'ZSW', age: 18},
+		]
+
+	};
+
+
+	render() {
+		const wellStyles = { maxWidth: 400, margin: '0 auto 10px' };
+		return (
+			<div className={'App'}>
+				{this.state.people.map(person => {
+					return (
+						<div className={'well'} style={wellStyles} key={person.name}>
+							<h2>{person.name}</h2>
+							<p>{person.age}</p>
+						</div>
+					)
+				})}
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/Components/PeopleInfo.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class PeopleInfo extends Component {
+	render() {
+		return (
+			<div>
+
+			</div>
+		);
+	}
+}
+
+
+export default PeopleInfo;
