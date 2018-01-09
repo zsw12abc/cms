f1b93f372907f6584c3bfa8a63de13c6f295c307
Shaowei Zhang <zsw22@vip.qq.com>
2018-01-09 15:15:53 +1100

    add high order component prepare for the lazy loading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import {BrowserRouter} from 'react-router-dom';
+
 import './App.css'
 import MainContent from './containers/MainContent'
 import NavigationBar from "./containers/Navigation";
@@ -7,15 +9,17 @@ import Drawer from "./containers/Drawer/Drawer";
 class App extends Component {
 	render() {
 		return (
-			<div>
-				<header><NavigationBar/></header>
-				<div className={'container-fluid'}>
-					<div className={'row flex-xl-nowrap'}>
-						<Drawer/>
-						<MainContent/>
+			<BrowserRouter basename={'/'}>
+				<div>
+					<header><NavigationBar/></header>
+					<div className={'container-fluid'}>
+						<div className={'row flex-xl-nowrap'}>
+							<Drawer/>
+							<MainContent/>
+						</div>
 					</div>
 				</div>
-			</div>
+			</BrowserRouter>
 		);
 	}
 }
